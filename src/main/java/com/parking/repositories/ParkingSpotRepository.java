1dfc54b11f0c39db9e278ee3924f89a407450f01
luiz fernando <luiz123jfmg@gmail.com>
2022-02-23 20:45:17 -0300

    feat: create service and controller

--- src/main/java/com/parking/repositories/ParkingSpotRepository.java
+++ src/main/java/com/parking/repositories/ParkingSpotRepository.java
@@ -3,9 +3,11 @@ package com.parking.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.parking.models.ParkingSpotModel;
 
+@Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
 }
