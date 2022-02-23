eacc4052a516197bf72887a56a5d60e62fbac362
luiz fernando <luiz123jfmg@gmail.com>
2022-02-23 20:38:30 -0300

    feat: create Parking Spot Model

--- src/main/java/com/parking/models/ParkingSpotModel.java
+++ src/main/java/com/parking/models/ParkingSpotModel.java
@@ -1,5 +1,109 @@
 package com.parking.models;
 
-public class ParkingSpotModel {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(name = "parking_spot_number", nullable = false, unique = true, length = 70)
+	private String parkingSpotNumber;
+	
+	@Column(name = "license_plate_car", nullable = false, unique = true, length = 7)
+	private String licensePlateCar;
+	
+	@Column(name = "brand_car", nullable = false, length = 70)
+	private String brandCar;
+	
+	@Column(name = "model_car", nullable = false, length = 70)
+	private String modelCar;
+	
+	@Column(name = "color_car", nullable = false, length = 70)
+	private String colorCar;
+	
+	@Column(name = "registration_date", nullable = false)
+	private LocalDateTime registrationDate;
+	
+	@Column(name = "responsible_name", nullable = false, length = 130)
+	private String responsibleName;
+	
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getParkingSpotNumber() {
+		return parkingSpotNumber;
+	}
+
+	public void setParkingSpotNumber(String parkingSpotNumber) {
+		this.parkingSpotNumber = parkingSpotNumber;
+	}
+
+	public String getLicensePlateCar() {
+		return licensePlateCar;
+	}
+
+	public void setLicensePlateCar(String licensePlateCar) {
+		this.licensePlateCar = licensePlateCar;
+	}
+
+	public String getBrandCar() {
+		return brandCar;
+	}
+
+	public void setBrandCar(String brandCar) {
+		this.brandCar = brandCar;
+	}
+
+	public String getModelCar() {
+		return modelCar;
+	}
+
+	public void setModelCar(String modelCar) {
+		this.modelCar = modelCar;
+	}
+
+	public String getColorCar() {
+		return colorCar;
+	}
+
+	public void setColorCar(String colorCar) {
+		this.colorCar = colorCar;
+	}
+
+	public LocalDateTime getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDateTime registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getResponsibleName() {
+		return responsibleName;
+	}
+
+	public void setResponsibleName(String responsibleName) {
+		this.responsibleName = responsibleName;
+	}
+	
+	
 
 }
